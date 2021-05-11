52ec45d0335f47b275ed8a65803053f3bbe3b6cf
Sergey Vorobyev <syvorobyev@gmail.com>
2021-05-11 17:58:00 +0300

    moved prices to a separate script

--- picsData.js
+++ picsData.js
@@ -52,57 +52,47 @@ const picsUrls = {
         'https://thumb.tildacdn.com/tild3866-6663-4863-a133-343365646230/-/resize/200x/-/format/webp/Mask_Group-5.png'
     ],
     lite: [
+        'https://static.tildacdn.com/tild3030-3337-4632-b462-373865373666/Rectangle_10.png',
+        'https://static.tildacdn.com/tild3361-6435-4431-b536-316466623664/Mask_Group.png',
+        'https://static.tildacdn.com/tild3339-3666-4734-b931-326463393439/Rectangle_28.png',
+        'https://static.tildacdn.com/tild3734-3030-4862-b537-333165303136/Rectangle_29.png',
+        'https://static.tildacdn.com/tild3732-3234-4233-b030-636632353738/Rectangle_30.png',
         
-        'https://thumb.tildacdn.com/tild3039-3938-4431-b965-313339353566/-/resize/200x/-/format/webp/Rectangle_10.png',
-        'https://thumb.tildacdn.com/tild6166-6662-4563-a235-626638663864/-/resize/200x/-/format/webp/Rectangle_27.png',
-        'https://thumb.tildacdn.com/tild3430-6133-4834-a434-613133333463/-/resize/200x/-/format/webp/Rectangle_28.png',
-
-        'https://thumb.tildacdn.com/tild6633-3862-4364-a466-666563323738/-/resize/200x/-/format/webp/Rectangle_10-1.png',
-        'https://thumb.tildacdn.com/tild3533-3463-4764-a264-663636313937/-/resize/200x/-/format/webp/Rectangle_27-1.png',
-        'https://thumb.tildacdn.com/tild3832-3532-4032-a466-373438326264/-/resize/200x/-/format/webp/Rectangle_28-1.png',
-
-        'https://thumb.tildacdn.com/tild6566-3464-4366-a335-643066343634/-/resize/200x/-/format/webp/Rectangle_10-2.png',
-        'https://thumb.tildacdn.com/tild3931-6163-4730-b837-393134633263/-/resize/200x/-/format/webp/Rectangle_27-2.png',
-        'https://thumb.tildacdn.com/tild3636-3362-4634-b330-653930373762/-/resize/200x/-/format/webp/Rectangle_28-2.png',
-
-        'https://thumb.tildacdn.com/tild3636-3462-4262-b739-646664643736/-/resize/200x/-/format/webp/Rectangle_10-3.png',
-        'https://thumb.tildacdn.com/tild6664-6231-4636-b864-303665613765/-/resize/200x/-/format/webp/Rectangle_27-3.png',
-        'https://thumb.tildacdn.com/tild6565-3539-4662-a532-643732313339/-/resize/200x/-/format/webp/Rectangle_28-3.png',
-
-        'https://thumb.tildacdn.com/tild3665-3937-4930-b863-646334396566/-/resize/200x/-/format/webp/Rectangle_10-4.png',
-        'https://thumb.tildacdn.com/tild3237-3932-4531-a535-396162303632/-/resize/200x/-/format/webp/Rectangle_27-4.png',
-        'https://thumb.tildacdn.com/tild6538-3334-4431-a534-616664323835/-/resize/200x/-/format/webp/Rectangle_28-4.png',
-
-        'https://thumb.tildacdn.com/tild3034-3365-4330-b235-646331336236/-/resize/200x/-/format/webp/Rectangle_10-5.png',
-        'https://thumb.tildacdn.com/tild6335-3763-4539-b532-636266656234/-/resize/200x/-/format/webp/Rectangle_27-5.png',
-        'https://thumb.tildacdn.com/tild3331-3730-4238-a639-653566393263/-/resize/200x/-/format/webp/Rectangle_28-5.png',
-
-        'https://thumb.tildacdn.com/tild6666-3132-4339-b732-323563623836/-/resize/200x/-/format/webp/Rectangle_10.png',
-        'https://thumb.tildacdn.com/tild3639-3062-4837-a465-663365613162/-/resize/200x/-/format/webp/Rectangle_27-6.png',
-        'https://thumb.tildacdn.com/tild6531-3631-4866-b230-663931346162/-/resize/200x/-/format/webp/Rectangle_28.png',
-
-        'https://thumb.tildacdn.com/tild3536-6131-4261-a131-653939653264/-/resize/200x/-/format/webp/Rectangle_29.png',
-        'https://thumb.tildacdn.com/tild3261-6263-4435-b133-343764306539/-/resize/200x/-/format/webp/Rectangle_29-1.png',
-        'https://thumb.tildacdn.com/tild3530-3538-4230-a431-336363613933/-/resize/200x/-/format/webp/Rectangle_29-2.png',
-        'https://thumb.tildacdn.com/tild3839-3333-4662-b935-333035333834/-/resize/200x/-/format/webp/Rectangle_29-3.png',
-        'https://thumb.tildacdn.com/tild3134-6632-4131-a161-626232623238/-/resize/200x/-/format/webp/Rectangle_29-4.png',
-        'https://thumb.tildacdn.com/tild3237-3764-4937-a266-336433663862/-/resize/200x/-/format/webp/Rectangle_29-5.png',
-        'https://thumb.tildacdn.com/tild6665-6361-4634-b230-343730363233/-/resize/200x/-/format/webp/Rectangle_29-6.png',
-
-        'https://thumb.tildacdn.com/tild3461-3761-4537-b561-316331616364/-/resize/200x/-/format/webp/Rectangle_30.png',
-        'https://thumb.tildacdn.com/tild3432-6432-4566-a263-373537393837/-/resize/200x/-/format/webp/Rectangle_31.png',
-        'https://thumb.tildacdn.com/tild3832-6536-4561-a561-316634616163/-/resize/200x/-/format/webp/Rectangle_29.png',
-        'https://thumb.tildacdn.com/tild6234-3563-4135-b933-373830383339/-/resize/200x/-/format/webp/Mask_Group.png',
-        'https://thumb.tildacdn.com/tild3532-3930-4433-b263-316234323963/-/resize/200x/-/format/webp/Rectangle_30-1.png',
-        'https://thumb.tildacdn.com/tild3737-6433-4636-b830-316232316163/-/resize/200x/-/format/webp/Mask_Group-1.png',
-        'https://thumb.tildacdn.com/tild6164-3330-4663-a463-356236643364/-/resize/200x/-/format/webp/Rectangle_29-1.png',
-        'https://thumb.tildacdn.com/tild3865-6632-4939-b935-623230366437/-/resize/200x/-/format/webp/Mask_Group-2.png',
-        'https://thumb.tildacdn.com/tild6139-3137-4236-a636-343133323264/-/resize/200x/-/format/webp/Rectangle_29-2.png',
-        'https://thumb.tildacdn.com/tild6530-3238-4462-a330-373234613837/-/resize/200x/-/format/webp/Mask_Group-3.png',
-        'https://thumb.tildacdn.com/tild6465-6162-4435-b164-393339323632/-/resize/200x/-/format/webp/Rectangle_29-3.png',
-        'https://thumb.tildacdn.com/tild6235-3063-4764-b431-653032633834/-/resize/200x/-/format/webp/Mask_Group-4.png',
-        'https://thumb.tildacdn.com/tild6164-6333-4166-b231-353964386133/-/resize/200x/-/format/webp/Rectangle_29-4.png',
-        'https://thumb.tildacdn.com/tild3866-6663-4863-a133-343365646230/-/resize/200x/-/format/webp/Mask_Group-5.png'
+        'https://static.tildacdn.com/tild3830-3162-4133-b962-376338313439/Rectangle_10.png',
+        'https://static.tildacdn.com/tild3337-3465-4234-a637-613562393564/Mask_Group.png',
+        'https://static.tildacdn.com/tild3565-3731-4863-a639-353961313630/Rectangle_28.png',
+        'https://static.tildacdn.com/tild6663-6634-4163-b334-363531623835/Rectangle_29.png',
+        'https://static.tildacdn.com/tild3336-6433-4339-a332-336533313661/Rectangle_30.png',
+        
+        'https://static.tildacdn.com/tild6337-6438-4663-b430-396161366162/Rectangle_10.png',
+        'https://static.tildacdn.com/tild6135-6632-4734-b264-653430623265/Mask_Group.png',
+        'https://static.tildacdn.com/tild3266-3831-4966-a439-363632396363/Rectangle_28.png',
+        'https://static.tildacdn.com/tild3162-6535-4330-b966-386362303537/Rectangle_29.png',
+        'https://static.tildacdn.com/tild3964-6136-4564-b962-383735653938/Rectangle_30.png',
+        
+        'https://static.tildacdn.com/tild3935-6439-4532-b134-316339393163/Rectangle_10.png',
+        'https://static.tildacdn.com/tild3730-3836-4663-a432-623461336563/Mask_Group.png',
+        'https://static.tildacdn.com/tild3363-3233-4934-b961-343264616139/Rectangle_28.png',
+        'https://static.tildacdn.com/tild3163-3637-4938-b662-623335663232/Rectangle_29.png',
+        'https://static.tildacdn.com/tild3333-3835-4163-b830-353032383663/Rectangle_30.png',
+        
+        'https://static.tildacdn.com/tild3535-6166-4733-b862-313231323063/Rectangle_10.png',
+        'https://static.tildacdn.com/tild3865-3733-4561-a366-616563623165/Mask_Group.png',
+        'https://static.tildacdn.com/tild3930-3133-4937-b733-633837663033/Rectangle_28.png',
+        'https://static.tildacdn.com/tild3361-3866-4533-a265-373263343238/Rectangle_29.png',
+        'https://static.tildacdn.com/tild3464-3332-4538-b535-636533336461/Rectangle_30.png',
+        
+        'https://static.tildacdn.com/tild6466-3664-4462-a232-353630393736/Rectangle_10.png',
+        'https://static.tildacdn.com/tild3164-6630-4036-a632-643437646435/Mask_Group.png',
+        'https://static.tildacdn.com/tild3362-3865-4163-a638-353734656132/Rectangle_28.png',
+        'https://static.tildacdn.com/tild6434-6663-4436-a262-356236666637/Rectangle_29.png',
+        'https://static.tildacdn.com/tild6437-6466-4939-a366-663466623235/Rectangle_30.png',
+        
+        'https://static.tildacdn.com/tild6265-3538-4133-b066-663832633730/Rectangle_10.png',
+        'https://static.tildacdn.com/tild3936-6332-4165-b633-303961383935/Mask_Group.png',
+        'https://static.tildacdn.com/tild3939-6165-4561-b463-303432623938/Rectangle_28.png',
+        'https://static.tildacdn.com/tild3966-3937-4137-b532-313131666431/Rectangle_29.png',
+        'https://static.tildacdn.com/tild3135-3339-4631-b262-613963383639/Rectangle_30.png'
     ],
     avan: [
 
